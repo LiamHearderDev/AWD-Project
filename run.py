cb437872c31702198cda33ac4c1558e657c7b2df
Lemon <158447112+LiamHearderDev@users.noreply.github.com>
2025-04-19 17:12:44 +0800

    Removed references to "create_app()" from run.py
    - Removed call for "create_app()"
    - renamed "app" variable to "application"
    - no longer importing "create_app()" from "app"

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from app import create_app
-
-# Create the Flask app using the factory function
-app = create_app()
+from app import application
 
 # Run the app if this script is executed directly
 if __name__ == "__main__":
