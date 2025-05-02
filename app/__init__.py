7ce75bba22b1449ffc38cea9c249460d8f4cf008
Lemon <LiamHearderDev@gmail.com>
2025-05-02 14:29:15 +0800

    Implemented Flask-WTF Form
    - Used Flask-WTF custom class form "FormLogin" in the login page.

--- app/__init__.py
+++ app/__init__.py
@@ -3,21 +3,24 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
 from flask_login import LoginManager
+
+
+
+
+application = Flask(__name__)
+application.config.from_object(Config)
+application.config['SECRET_KEY'] = 'you-will-never-guess'
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
+login = LoginManager(application)
+login.login_view = 'login'
+
 from app import models
 import app.routes.auth_routes
 import app.routes.game_routes
 import app.routes.intro_routes
 import app.routes.main_routes
 import app.routes.stats_routes
-
-
-application = Flask(__name__)
-application.config.from_object(Config)
-db = SQLAlchemy(application)
-migrate = Migrate(application, db)
-login = LoginManager(application)
-login.login_view = 'login'
-
 
 if __name__ == '__main__':
     application.run(debug=True)
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,12 +3,14 @@ from flask import render_template
 from flask_login import current_user, login_user
 import sqlalchemy as sa
 from app.models import User
+from app.forms import LoginForm
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return render_template('auth/intro.html')
-    
+        return render_template('intro/intro.html')
+    form = LoginForm()
+    return render_template('auth/login.html', form=form)
 
 @application.route('/register', methods=['GET', 'POST'])
 def register():
