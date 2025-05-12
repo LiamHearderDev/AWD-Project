bf6b048301e1ce737e0710b246ec6c66107086f4
Ryan <201040489+Ryan1090@users.noreply.github.com>
2025-05-12 14:35:48 +0800

    added tests for correct rendering and retrieving from paragraph table: test if correct html loaded test if empty paragraph tabkle handled correctly test if paragraph successfully retrieved from database

--- tests/unit/game_tests.py
+++ tests/unit/game_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from app import create_app, db
 from app.config import TestingConfig
+from app.models import Paragraph
 
 class GameBlueprintTestCase(unittest.TestCase):
     def setUp(self):
@@ -16,7 +17,31 @@ class GameBlueprintTestCase(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    
+    def test_game_route_renders_template(self): # make sure correct template is successfully sent
+        response = self.client.get('/game')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'gameElement', response.data) # check if element with id=gameElement exists, which it should
 
+    def test_random_paragraph_no_content(self): # try get paragraph from empty table
+        response = self.client.get('/random-paragraph')
+        self.assertEqual(response.status_code, 404)
+        data = response.get_json()
+        self.assertEqual(data, {'error': 'no paragraphs available'})
+
+    def test_random_paragraph_success(self): # make sure paragraph is successfully retrieved
+        with self.app.app_context(): # add paragraph
+            p = Paragraph(body='Test paragraph', type='test')
+            db.session.add(p)
+            db.session.commit()
+            pid = p.paragraph_id
+        response = self.client.get('/random-paragraph') # get paragraph
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        # still same values
+        self.assertEqual(data['paragraph_id'], pid)
+        self.assertEqual(data['body'], 'Test paragraph')
+        self.assertEqual(data['type'], 'test')
+
+    
 if __name__ == '__main__':
     unittest.main()
